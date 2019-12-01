7aa3443eb60323d571f360d696d69fbcd413fd20
Alan Baines <abaines@vt.edu>
2019-12-01 17:03:37 -0500

    color settings and locale

--- settings.lua
+++ settings.lua
@@ -21,5 +21,38 @@ data:extend({
 		maximum_value = 32,
 		setting_type = "runtime-global",
 	},
+	-- color
+	{
+		type = "double-setting",
+		name = "show-tite-grid-color-r",
+		default_value = 0.35,
+		minimum_value = 0,
+		maximum_value = 1,
+		setting_type = "runtime-global",
+	},
+	{
+		type = "double-setting",
+		name = "show-tite-grid-color-g",
+		default_value = 0.35,
+		minimum_value = 0,
+		maximum_value = 1,
+		setting_type = "runtime-global",
+	},
+	{
+		type = "double-setting",
+		name = "show-tite-grid-color-b",
+		default_value = 0.35,
+		minimum_value = 0,
+		maximum_value = 1,
+		setting_type = "runtime-global",
+	},
+	{
+		type = "double-setting",
+		name = "show-tite-grid-color-a",
+		default_value = 0.35,
+		minimum_value = 0,
+		maximum_value = 1,
+		setting_type = "runtime-global",
+	},
 })
 
