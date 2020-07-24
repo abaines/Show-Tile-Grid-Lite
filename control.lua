c582c7cd8474e65b3c44cd956b6f47799b388c69
Alan Baines <abaines@vt.edu>
2020-07-24 00:01:29 -0400

    add nil check

--- control.lua
+++ control.lua
@@ -10,7 +10,7 @@ end
 
 
 local function checkPlayerCursor(player)
-	local hasSomethingOnCursor = player.cursor_stack.valid_for_read or (player.cursor_ghost and player.cursor_ghost.valid)
+	local hasSomethingOnCursor = (player.cursor_stack and player.cursor_stack.valid_for_read) or (player.cursor_ghost and player.cursor_ghost.valid)
 	return hasSomethingOnCursor
 end
 
